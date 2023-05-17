c07c742896c1c61e2ac7756cf642d785cd3616c2
semio <semio@posteo.net>
2023-05-17 23:35:17 +0800

    add openai wrapper

---
+++ automation-api/lib/llms/openai.py
@@ -0,0 +1,67 @@
+"""Wrapper around LangChain's openAI wrapper.
+"""
+
+from dataclasses import dataclass, field
+from typing import Optional, Union
+
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+from lib.config import read_config
+
+
+@dataclass
+class OpenAIModel:
+    """get openAI model by name.
+
+    example:
+
+    >>> model = OpenAIModel('gpt-3.5-turbo')
+    >>> template = "What are the top {k} resources to learn {this} in 2023?"
+    >>> model.run(template, k=3, this='rust')
+
+    By default the api key and organization defined in .env will be used.
+    But you can specify a different api key or organization:
+
+    >>> model = OpenAIModel('gpt-3.5-turbo', api_key='...', org_id='...')
+    """
+
+    model_name: str
+    api_key: Optional[str] = field(default=None, repr=False)
+    org_id: Optional[str] = field(default=None, repr=False)
+    _model: Union[OpenAI, ChatOpenAI] = field(init=False, default=None, repr=False)
+
+    def __post_init__(self):
+        "docstring"
+        config = read_config()
+        if self.api_key is None:
+            self.api_key = config["OPENAI_API_KEY"]
+        if self.org_id is None:
+            self.org_id = config["OPENAI_ORG_ID"]
+
+        if self.model_name in ["gpt-3.5-turbo", "gpt-4"]:
+            self._model = ChatOpenAI(
+                model_name=self.model_name,
+                openai_api_key=self.api_key,
+                openai_organization=self.org_id,
+            )
+        else:
+            self._model = OpenAI(
+                model_name=self.model_name,
+                openai_api_key=self.api_key,
+                openai_organization=self.org_id,
+            )
+
+    @property
+    def model(self):
+        return self._model
+
+    def run(self, prompt_text, **kwargs):
+        prompt = PromptTemplate(
+            template=prompt_text, input_variables=list(kwargs.keys())
+        )
+        chain = LLMChain(llm=self.model, prompt=prompt)
+
+        return chain.run(kwargs)
