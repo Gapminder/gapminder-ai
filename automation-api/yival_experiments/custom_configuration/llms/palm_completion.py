fd4de969550e28fdf7940fd3f245bd35623136af
Motin <motin@motin.eu>
2024-02-01 17:13:08 +0200

    Fix typo, the new safety categories were the old ones, now they are the new ones, making Gemini requests work again

--- automation-api/yival_experiments/custom_configuration/llms/palm_completion.py
+++ automation-api/yival_experiments/custom_configuration/llms/palm_completion.py
@@ -33,31 +33,19 @@ safety_settings_old_categories = [
 
 safety_settings_new_categories = [
     {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_TOXICITY,
+        "category": generativeai.types.HarmCategory.HARM_CATEGORY_HARASSMENT,
         "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
     },
     {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_SEXUAL,
+        "category": generativeai.types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
         "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
     },
     {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_MEDICAL,
+        "category": generativeai.types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
         "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
     },
     {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_DEROGATORY,
-        "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_DANGEROUS,
-        "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-        "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-        "category": generativeai.types.HarmCategory.HARM_CATEGORY_VIOLENCE,
+        "category": generativeai.types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
         "threshold": generativeai.types.HarmBlockThreshold.BLOCK_NONE,
     },
 ]
