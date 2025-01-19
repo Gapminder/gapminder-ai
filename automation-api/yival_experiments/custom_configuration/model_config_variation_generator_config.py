2585ce678590b0df744f81849ab6842f1585a536
semio <semio@posteo.net>
2025-01-19 10:28:43 +0800

    remove yival folder

--- automation-api/yival_experiments/custom_configuration/model_config_variation_generator_config.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Any, Dict, List, Optional
-
-from yival.schemas.varation_generator_configs import BaseVariationGeneratorConfig
-
-
-@dataclass
-class ModelConfigVariationGeneratorConfig(BaseVariationGeneratorConfig):
-    models: Optional[List[Dict[str, Any]]] = None  # List of variations to generate
